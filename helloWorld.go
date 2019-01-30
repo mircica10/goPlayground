2ea317b6875dc540da004ca2b465d1fe2076df80
baby A <mircica10@yahoo.com>
2019-01-30 23:20:31 +0100

    variables

--- helloWorld.go
+++ helloWorld.go
@@ -3,6 +3,6 @@ package main
 
 import "fmt"
 
-func main() {
+func helloWorld() {
 	fmt.Println("hello world")
 }
---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func varibles() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	e := "short"
+
+	fmt.Println(e)
+
+	var f int
+	f = 6
+	fmt.Println(f)
+
+}
